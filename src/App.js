f5dc9be4bc0a16881cb2a2f108fb11db55ce7a37
Victor <victor.porras.work@gmail.com>
2023-04-19 10:47:18 +0200

    mki rama

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import NavBar from './components/NavBar';
 >>>>>>> 609e008 (upd structure)
@@ -69,6 +70,8 @@ const [absnoprevistas, setAbsnoprevistas] = useState([]);
 =======
 =======
 =======
+>>>>>>> ddc6482 (mki rama)
+=======
 <<<<<<< HEAD
 import NavBar from './NavBar';
 import { useEffect, useState } from 'react';
@@ -86,13 +89,60 @@ import BaixesMedComponent from './components/BaixesMedComponent';
 import SortidesCurComponent from './components/SortidesCurComponent';
 
 document.title = "Projecte M12";
+=======
+
+import NavBar from './components/NavBar';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+>>>>>>> d111168 (mki rama)
 
 function App() {
+<<<<<<< HEAD
 >>>>>>> a848aa9 (compos)
+=======
+<<<<<<< HEAD
+=======
+/* 
+  // Llista absnoprevistes
+  const [llista,setLlistaNoAbs] = useState([]);
+  // Error que s'ha produït
+  const [error, setError] = useState(null);
+  // Element nou o que s'actualitza 
+  const [item, setItem] = useState({data_absnoprevista:"", _id:"", hores_ausencia:"", motiu_abs: "wate wate", document_justificatiu:""});
+
+ 
+  useEffect( () => {
+
+    axios.get('http://localhost:5000/api/absnoprevistes/all')
+      .then((response) => {
+          setLlistaNoAbs(response.data);
+          console.log(response.data)
+          }).catch(error => {
+            setError(error);
+          });
+},[]);
+*/
+const [absnoprevistas, setAbsnoprevistas] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {     
+      const result = await axios.get('http://localhost:5000/api/absnoprevistes/all');
+      setAbsnoprevistas(result.data)
+    } catch (error) {
+      console.log(error);
+      // Aquí puedes agregar un mensaje de error en la página o tomar otras acciones
+    }
+  };
+  fetchData();
+}, []);
+>>>>>>> d111168 (mki rama)
+>>>>>>> ddc6482 (mki rama)
 
   return (
     <div className="App">
       <NavBar />
+<<<<<<< HEAD
 <<<<<<< HEAD
       {absnoprevistas.map((absnoprevista) => (
       <div key={absnoprevista._id}>
@@ -104,6 +154,8 @@ function App() {
       </div>
     ))}
 =======
+=======
+>>>>>>> ddc6482 (mki rama)
       <div className="container">
         <h3>Abs no previstes tmp</h3>
         <AbsNoPreComponent />
@@ -114,7 +166,21 @@ function App() {
         <h3>Sortides curriculars tmp</h3>
         <SortidesCurComponent />
       </div>
+<<<<<<< HEAD
 >>>>>>> a848aa9 (compos)
+=======
+=======
+      {absnoprevistas.map((absnoprevista) => (
+      <div key={absnoprevista._id}>
+        <p>Data Absnoprevista: {absnoprevista.data_absnoprevista}</p>
+        <p>ID: {absnoprevista._id}</p>
+        <p>Hores d'absència: {absnoprevista.hores_ausencia}</p>
+        <p>Motiu d'absència: {absnoprevista.motiu_abs}</p>
+        <p>Document justificatiu: {absnoprevista.document_justificatiu}</p>
+      </div>
+    ))}
+>>>>>>> d111168 (mki rama)
+>>>>>>> ddc6482 (mki rama)
     </div>
   );
 
