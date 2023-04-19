a848aa9cd74abbd42bbdb93fef7513c5e0920055
josep <bfg9kappa@gmail.com>
2023-04-19 16:44:13 +0200

    compos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import { useEffect, useState } from 'react';
-import axios from "axios";
-  
-function App() {
 
-  // Llista absnoprevistes
-  const [] = useState([]);
-  // Error que s'ha produït
-  const [error, setError] = useState(null);
-  // Element nou o que s'actualitza
-  const [llista,setLlistaNoAbs] = useState({
-    _id:"",
-    data_absnoprevista:"",
-    hores_ausencia:"",
-    motiu_abs: "",
-    document_justificatiu:""
-  });
-  
-  useEffect( () => {
-    axios.get('http://localhost:5000/api/absnoprevistes/all')
-      .then((response) => {
-        setLlistaNoAbs(response.data);
-        console.log(response.data)
-      }).catch(error => {
-        setError(error);
-      });
-  },[]);
+import NavBar from './components/NavBar';
+import AbsNoPreComponent from './components/AbsNoPreComponent';
+import AbsPreComponent from './components/AbsPreComponent';
+
+function App() {
 
   return (
     <div className="App">
       <NavBar />
-      <h1>Yeeepa</h1>
-      
+      <div className="container">
+        <h3>Abs no previstes xdd</h3>
+        <AbsNoPreComponent />
+        <h3>Abs pre LOL</h3>
+        <AbsPreComponent />
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/AbsPreComponent.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function AbsPreComponent() {
+  const [absData, setAbsData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/absprevistes/all')
+      .then((response) => {
+        setAbsData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {absData.map((item, index) => (
+        <div key={index}>
+          <p>{item._id}</p>
+          <p>{item.data_absnoprevista}</p>
+          <p>{item.hores_ausencia}</p>
+          <p>{item.motiu_abs}</p>
+          <p>{item.document_justificatiu}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AbsPreComponent;
