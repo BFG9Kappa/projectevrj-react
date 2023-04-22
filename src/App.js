b7b2b62afb07fc05dd13038b659f1c32d655debe
Victor <victor.porras.work@gmail.com>
2023-04-22 13:51:28 +0200

    Absnoprevistes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AbsPrePage from './pages/AbsPrePage';
-import AbsNoPrePage from './pages/AbsNoPrePage';
+import AbsNoPrePage from './pages/AbsNoPrevistes/AbsNoPrePage';
+import AbsNoPrePageNew from './pages/AbsNoPrevistes/AbsNoPrePageNew';
 import BaixesMedPage from './pages/BaixesMedPage';
 import SortidesCurPage from './pages/SortidesCurPages';
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/absprevistes" component={AbsPrePage} />
           <Route path="/absnoprevistes" component={AbsNoPrePage} />
+          <Route path="/absnoprevistes/new" component={AbsNoPrePageNew} />
           <Route path="/baixesmediques" component={BaixesMedPage} />
           <Route path="/sortidescurriculars" component={SortidesCurPage} />
         </div>
