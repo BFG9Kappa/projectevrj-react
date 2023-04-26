cd7da9dbc95baf22baf71ee4cf4bb4f2c979ffd8
bfg9kappa <bfg9kappa@gmail.com>
2023-04-26 10:03:00 +0200

    upp

--- src/components/AbsNoPrevistes/AbsNoPreComponentCreate.jsx
+++ src/components/AbsNoPrevistes/AbsNoPreComponentCreate.jsx
@@ -1,35 +1,39 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 function AbsNoPreComponentCreate() {
   const [AbsNoPreData, setAbsNoPreData] = useState([]);
   const [formData, setFormData] = useState({
-    data_absnoprevista: '',
-    hores_ausencia: '',
-    motiu_abs: '',
-    document_justificatiu: '',
-    user: ''
+    data_absnoprevista: "",
+    hores_ausencia: "",
+    motiu_abs: "",
+    document_justificatiu: "",
+    user: "",
   });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:5000/api/absnoprevistes/create/', formData)
+    axios
+      .post("http://localhost:5000/api/absnoprevistes/create/", formData)
       .then((response) => {
         console.log(response.data);
         setFormData({
-          data_absnoprevista: '',
-          hores_ausencia: '',
-          motiu_abs: '',
-          document_justificatiu: '',
-          user: ''
+          data_absnoprevista: "",
+          hores_ausencia: "",
+          motiu_abs: "",
+          document_justificatiu: "",
+          user: "",
         });
         setAbsNoPreData([...AbsNoPreData, response.data]);
       })
@@ -40,54 +44,60 @@ function AbsNoPreComponentCreate() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Data absència no prevista:</label>
-          <input
+      <Form onSubmit={handleSubmit}>
+
+        <InputGroup className="mb-2">
+          <InputGroup.Text>Data absència</InputGroup.Text>
+          <Form.Control
             type="date"
             name="data_absnoprevista"
             value={formData.data_absnoprevista}
             onChange={handleInputChange}
           />
-        </div>
-        <div>
-          <label>Hores d'absència:</label>
-          <input
+        </InputGroup>
+
+        <InputGroup className="mb-2">
+          <InputGroup.Text>Hores d'absència</InputGroup.Text>
+          <Form.Control
             type="number"
             name="hores_ausencia"
             value={formData.hores_ausencia}
             onChange={handleInputChange}
           />
-        </div>
-        <div>
-          <label>Motiu de l'absència:</label>
-          <input
-            type="text"
+        </InputGroup>
+
+        <InputGroup className="mb-2">
+          <InputGroup.Text>Motiu absència</InputGroup.Text>
+          <Form.Control
+            as="textarea"
             name="motiu_abs"
             value={formData.motiu_abs}
             onChange={handleInputChange}
           />
-        </div>
-        <div>
-          <label>Document justificatiu:</label>
-          <input
+        </InputGroup>
+
+        <InputGroup className="mb-2">
+          <InputGroup.Text>Document justificatiu</InputGroup.Text>
+          <Form.Control
             type="text"
             name="document_justificatiu"
             value={formData.document_justificatiu}
             onChange={handleInputChange}
           />
-        </div>
-        <div>
-          <label>Usuari:</label>
-          <input
+        </InputGroup>
+
+        <InputGroup className="mb-2">
+          <InputGroup.Text>Professor</InputGroup.Text>
+          <Form.Control
             type="text"
             name="user"
             value={formData.user}
             onChange={handleInputChange}
           />
-        </div>
-        <button type="submit">Crear Absència</button>
-      </form>
+        </InputGroup>
+
+        <Button type="submit">Enviar</Button>
+      </Form>
     </>
   );
 }
