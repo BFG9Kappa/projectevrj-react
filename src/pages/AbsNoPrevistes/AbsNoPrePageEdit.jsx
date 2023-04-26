0756803761aebb19e007a639027752b9f7576f8e
Victor <victor.porras.work@gmail.com>
2023-04-26 21:49:28 +0200

    UPDATE absnoprevistes

---
+++ src/pages/AbsNoPrevistes/AbsNoPrePageEdit.jsx
@@ -0,0 +1,17 @@
+import NavBar from '../../components/NavBar';
+import AbsNoPreComponentUpdate from '../../components/AbsNoPrevistes/AbsNoPreComponentUpdate';
+
+function AbsNoPrePageEdit() {
+
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h3>Editar absència no prevista</h3>
+        <AbsNoPreComponentUpdate />
+      </div>
+    </>
+  );
+}
+
+export default AbsNoPrePageEdit;
