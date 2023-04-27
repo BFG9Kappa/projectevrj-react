7d8634f10b20311c3326d2bdab8d9d4ad24aa3d1
Victor <victor.porras.work@gmail.com>
2023-04-27 08:57:07 +0200

    edit hora inici hora fi

--- src/components/AbsNoPrevistes/AbsNoPreComponentUpdate.jsx
+++ src/components/AbsNoPrevistes/AbsNoPreComponentUpdate.jsx
@@ -11,7 +11,8 @@ function AbsNoPreComponentUpdate() {
 
   const [item, setItem] = useState({
     data_absnoprevista: "",
-    hores_ausencia: "",
+    hora_inici_absnoprevista: "",
+    hora_final_absnoprevista: "",
     motiu_abs: "",
     document_justificatiu: "",
     user: "",
@@ -46,6 +47,14 @@ function AbsNoPreComponentUpdate() {
     });
   };
 
+  const handleSelectChange = (event) => {
+    const { name, value } = event.target;
+    setItem({
+      ...item,
+      [name]: value,
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
@@ -72,15 +81,57 @@ function AbsNoPreComponentUpdate() {
           />
         </InputGroup>
 
-        <InputGroup className="mb-2">
-          <InputGroup.Text>Hores d'absència</InputGroup.Text>
-          <Form.Control
-            type="number"
-            name="hores_ausencia"
-            value={item.hores_ausencia}
-            onChange={handleInputChange}
-          />
-        </InputGroup>
+        <Form.Select
+          name="hora_inici_absnoprevista"
+          aria-label="hora_inici_absnoprevista"
+          value={item.hora_inici_absnoprevista}
+          onChange={handleSelectChange}
+        >
+          <option>Hora inici absència</option>
+          <optgroup label="Diurn">
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="11:30">11:30</option>
+            <option value="12:30">12:30</option>
+            <option value="13:30">13:30</option>
+          </optgroup>
+          <optgroup label="Nocturn">
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:30">18:30</option>
+            <option value="19:30">19:30</option>
+            <option value="20:30">20:30</option>
+          </optgroup>
+        </Form.Select>
+
+        <Form.Select
+          name="hora_final_absnoprevista"
+          aria-label="hora_final_absnoprevista"
+          value={item.hora_final_absnoprevista}
+          onChange={handleSelectChange}
+        >
+          <option>Hora final absència</option>
+          <optgroup label="Diurn">
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="11:30">11:30</option>
+            <option value="12:30">12:30</option>
+            <option value="13:30">13:30</option>
+          </optgroup>
+          <optgroup label="Nocturn">
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:30">18:30</option>
+            <option value="19:30">19:30</option>
+            <option value="20:30">20:30</option>
+          </optgroup>
+        </Form.Select>
 
         <InputGroup className="mb-2">
           <InputGroup.Text>Motiu absència</InputGroup.Text>
